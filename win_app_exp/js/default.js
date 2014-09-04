29cbe01e7496e2da506c72488400b5ef7cdf89ca
Kris Walker <kris@kixx.name>
2014-09-04 01:24:10 -0400

    New navigational app template

--- win_app_exp/js/default.js
+++ win_app_exp/js/default.js
@@ -1,49 +1,45 @@
-﻿(function () {
-  "use strict";
+﻿// For an introduction to the Navigation template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232506
+(function () {
+    "use strict";
 
-  WinJS.Binding.optimizeBindingReferences = true;
+    WinJS.Binding.optimizeBindingReferences = true;
 
-  var app = WinJS.Application;
-  var activation = Windows.ApplicationModel.Activation;
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
 
-  app.onactivated = function (args) {
-    if (args.detail.kind === activation.ActivationKind.launch) {
-      if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-        // TODO: This application has been newly launched. Initialize
-        // your application here.
-      } else {
-        // TODO: This application has been reactivated from suspension.
-        // Restore application state here.
-      }
-      args.setPromise(WinJS.UI.processAll().then(function () {
-        var helloButton = document.getElementById('hello-button')
-          , ratingControl = document.getElementById('rating-control-div').winControl
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
 
-        helloButton.addEventListener('click', buttonClickHandler, false);
-        ratingControl.addEventListener('change', ratingChanged, false);
-      }));
-    }
-  };
+            if (app.sessionState.history) {
+                nav.history = app.sessionState.history;
+            }
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
+                } else {
+                    return nav.navigate(Application.navigator.home);
+                }
+            }));
+        }
+    });
 
-  app.oncheckpoint = function (args) {
-    // TODO: This application is about to be suspended. Save any state
-    // that needs to persist across suspensions here. You might use the
-    // WinJS.Application.sessionState object, which is automatically
-    // saved and restored across suspension. If you need to complete an
-    // asynchronous operation before your application is suspended, call
-    // args.setPromise().
-  };
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
+    };
 
-  function buttonClickHandler(ev) {
-    var userName = document.getElementById('name-input').value
-      , greetingString = "Hello, "+ userName +"!"
-    document.getElementById('greeting-output').innerText = greetingString;
-  }
-
-  function ratingChanged(ev) {
-    var ratingOutput = document.getElementById('rating-output')
-    ratingOutput.innerText = ev.detail.tentativeRating;
-  }
-
-  app.start();
+    app.start();
 })();
