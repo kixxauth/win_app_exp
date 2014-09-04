2dba04c7753a890864b83b7858f6ca1b501782e0
Kris Walker <kris@kixx.name>
2014-09-04 01:21:08 -0400

    Dynamic user interaction

--- win_app_exp/js/default.js
+++ win_app_exp/js/default.js
@@ -1,34 +1,49 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
-(function () {
-    "use strict";
+﻿(function () {
+  "use strict";
 
-    WinJS.Binding.optimizeBindingReferences = true;
+  WinJS.Binding.optimizeBindingReferences = true;
 
-    var app = WinJS.Application;
-    var activation = Windows.ApplicationModel.Activation;
+  var app = WinJS.Application;
+  var activation = Windows.ApplicationModel.Activation;
 
-    app.onactivated = function (args) {
-        if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize
-                // your application here.
-            } else {
-                // TODO: This application has been reactivated from suspension.
-                // Restore application state here.
-            }
-            args.setPromise(WinJS.UI.processAll());
-        }
-    };
+  app.onactivated = function (args) {
+    if (args.detail.kind === activation.ActivationKind.launch) {
+      if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+        // TODO: This application has been newly launched. Initialize
+        // your application here.
+      } else {
+        // TODO: This application has been reactivated from suspension.
+        // Restore application state here.
+      }
+      args.setPromise(WinJS.UI.processAll().then(function () {
+        var helloButton = document.getElementById('hello-button')
+          , ratingControl = document.getElementById('rating-control-div').winControl
 
-    app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
-    };
+        helloButton.addEventListener('click', buttonClickHandler, false);
+        ratingControl.addEventListener('change', ratingChanged, false);
+      }));
+    }
+  };
 
-    app.start();
+  app.oncheckpoint = function (args) {
+    // TODO: This application is about to be suspended. Save any state
+    // that needs to persist across suspensions here. You might use the
+    // WinJS.Application.sessionState object, which is automatically
+    // saved and restored across suspension. If you need to complete an
+    // asynchronous operation before your application is suspended, call
+    // args.setPromise().
+  };
+
+  function buttonClickHandler(ev) {
+    var userName = document.getElementById('name-input').value
+      , greetingString = "Hello, "+ userName +"!"
+    document.getElementById('greeting-output').innerText = greetingString;
+  }
+
+  function ratingChanged(ev) {
+    var ratingOutput = document.getElementById('rating-output')
+    ratingOutput.innerText = ev.detail.tentativeRating;
+  }
+
+  app.start();
 })();
